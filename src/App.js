917e126c8f091d771a820e572d504942a40a1bbe
Julian Berger <jul.ber04@gmail.com>
2022-01-06 09:47:06 +0100

    Added API-Key

--- src/App.js
+++ src/App.js
@@ -4,11 +4,14 @@ import Content from "./components/Content";
 
 const App = () => {
 
-    const titleText = 'Weather App - by Juan'
+    const api = {
+        key: '67b6be7208461828ada5f8350f08c711',
+        base: 'https://api.openweathermap.org/data/2.5'
+    }
 
     return (
         <>
-            <Header titleText={titleText}/>
+            <Header/>
             <Content/>
         </>
     );
