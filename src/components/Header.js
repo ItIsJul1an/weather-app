239b90c316559db20dbf7ae323c1fdf4165d785c
Julian Berger <jul.ber04@gmail.com>
2022-01-06 09:47:41 +0100

    Added Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useRef} from 'react';
+import logo from '../resources/assets/weather-app-icon.svg'
+import search_icon from '../resources/assets/search-icon.svg'
 
-const Header = ({titleText}) => {
+const Header = () => {
+
+    const inputRef = useRef()
 
     return (
         <>
-            <h1 id='title'>{titleText}</h1>
+            <div className='main-container'>
+                <div className='app-info-container'>
+                    <img src={logo} alt={'App-Logo'} className='app-logo'/>
+                    <label className='app-label'>Wetter App</label>
+                </div>
+                <div className='search-container'>
+                    <input type='text' ref={inputRef} placeholder='Stadtnamen eingeben ...' className='input-field'/>
+                    <img src={search_icon} alt={'Search-Icon'} className='search-icon'/>
+                </div>
+            </div>
         </>
     );
 };
