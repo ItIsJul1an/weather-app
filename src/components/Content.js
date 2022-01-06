514797373da423d6d6b708c7db57fa6a51182600
Julian Berger <jul.ber04@gmail.com>
2022-01-06 09:47:49 +0100

    Added Content

--- src/components/Content.js
+++ src/components/Content.js
@@ -2,9 +2,20 @@ import React from 'react';
 
 const Content = () => {
     return (
-        <div>
-            <h1 id='temp'>Website under construction!</h1>
-        </div>
+        <>
+            <div className='main-container'>
+                <div className='weather-info-container'>
+                    <h1>Perg Wetter</h1>
+                    <span>Stand 10:38 CET</span><br/>
+                    <span>4°C</span><br/>
+                    <span>Bedeckt</span><br/>
+                    <span>Regen</span>
+                </div>
+                <div className='weather-icon-container'>
+                    dsadsad
+                </div>
+            </div>
+        </>
     );
 };
 
