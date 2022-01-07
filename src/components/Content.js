d23bae27af301ae9e61f7309deaccc3fffd8412c
Julian Berger <jul.ber04@gmail.com>
2022-01-07 15:30:13 +0100

    Added new Content

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,18 +1,58 @@
 import React from 'react';
+import sunny from '../resources/assets/sunny-weather.png'
 
 const Content = () => {
+
     return (
         <>
             <div className='main-container'>
-                <div className='weather-info-container'>
-                    <h1>Perg Wetter</h1>
-                    <span>Stand 10:38 CET</span><br/>
-                    <span>4°C</span><br/>
-                    <span>Bedeckt</span><br/>
-                    <span>Regen</span>
+                <div className='content-container'>
+                    <div className='weather-info-container'>
+                        <span className='large-bold-font'>Perg Wetter</span>
+                        <span className='normal-font'>Stand 10:38 CET</span>
+                        <span className='large-bold-cursive-font'>4°C</span>
+                        <span className='normal-bold-font'>Bedeckt Regen</span>
+                    </div>
+                    <div className='weather-icon-container'>
+                        <img src={sunny} alt={'Weather-Icon'} className='weather-icon'/>
+                        <span className='normal-font'>Fühlt sich an wie 5°C</span>
+                    </div>
                 </div>
-                <div className='weather-icon-container'>
-                    dsadsad
+                <div className='content-container'>
+                    <div className='weather-info-container'>
+                        <span className='large-bold-font'>Perg Wetter</span>
+                        <span className='normal-font'>Stand 10:38 CET</span>
+                        <span className='large-bold-cursive-font'>4°C</span>
+                        <span className='normal-bold-font'>Bedeckt Regen</span>
+                    </div>
+                    <div className='weather-icon-container'>
+                        <img src={sunny} alt={'Weather-Icon'} className='weather-icon'/>
+                        <span className='normal-font'>Fühlt sich an wie 5°C</span>
+                    </div>
+                </div>
+                <div className='content-container'>
+                    <div className='weather-info-container'>
+                        <span className='large-bold-font'>Perg Wetter</span>
+                        <span className='normal-font'>Stand 10:38 CET</span>
+                        <span className='large-bold-cursive-font'>4°C</span>
+                        <span className='normal-bold-font'>Bedeckt Regen</span>
+                    </div>
+                    <div className='weather-icon-container'>
+                        <img src={sunny} alt={'Weather-Icon'} className='weather-icon'/>
+                        <span className='normal-font'>Fühlt sich an wie 5°C</span>
+                    </div>
+                </div>
+                <div className='content-container'>
+                    <div className='weather-info-container'>
+                        <span className='large-bold-font'>Perg Wetter</span>
+                        <span className='normal-font'>Stand 10:38 CET</span>
+                        <span className='large-bold-cursive-font'>4°C</span>
+                        <span className='normal-bold-font'>Bedeckt Regen</span>
+                    </div>
+                    <div className='weather-icon-container'>
+                        <img src={sunny} alt={'Weather-Icon'} className='weather-icon'/>
+                        <span className='normal-font'>Fühlt sich an wie 5°C</span>
+                    </div>
                 </div>
             </div>
         </>
